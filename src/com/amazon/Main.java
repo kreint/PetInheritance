ab206132e47f14d25025143fd4b27bbead70cec7
Tim Krein <kreint@amazon.com>
2021-08-02 16:31:11 -0400

    Added more for loops

--- src/com/amazon/Main.java
+++ src/com/amazon/Main.java
@@ -1,5 +1,7 @@
 package com.amazon;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +9,39 @@ public class Main {
         Cat cat = new Cat("Felix");
         Dog dog = new Dog("Fido");
 
+//        int[] inclusiveHours = IntStream.rangeClosed(0, 23).toArray();
+//        int[] exclusiveHours = IntStream.range(0, 24).toArray();
+
         for (int hour = 0; hour < 24; ++hour) {
             cat.performActivity();
             dog.performActivity();
         }
+
+//        int hour = 0;
+//        while (hour < 24) {
+//            cat.performActivity();
+//            dog.performActivity();
+//            ++hour;
+//        }
+
+//        int hour = 0;
+//        do {
+//            cat.performActivity();
+//            dog.performActivity();
+//            hour++;
+//        } while (hour < 24);
+
+//        for (int hour : inclusiveHours) {
+//            System.out.println("inclusive hour=" + hour);
+//            cat.performActivity();
+//            dog.performActivity();
+//        }
+//
+//        for (int hour : exclusiveHours) {
+//            System.out.println("exclusive hour=" + hour);
+//            cat.performActivity();
+//            dog.performActivity();
+//        }
 
     }
 }
